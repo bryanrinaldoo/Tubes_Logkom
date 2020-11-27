ebf7c7835fe44f029888223e7b2b73238125772b
slarkdarr <daffaresyaly@gmail.com>
2020-11-28 00:55:43 +0700

    Update quest

--- quest.pl
+++ quest.pl
@@ -57,91 +57,91 @@ killquest :-
 
     (Pickquest = 1 ->
     (Level >= 1 ->
-    asserta(quest(User,newbiemission1(X))),
+    asserta(quest(User,newbiemission1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 2 ->
     (Level >= 1 ->
-    asserta(quest(User,newbiemission2(X))),
+    asserta(quest(User,newbiemission2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 3 ->
     (Level >= 3 ->
-    asserta(quest(User,killogre1(X))),
+    asserta(quest(User,killogre1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 4 ->
     (Level >= 5 ->
-    asserta(quest(User,killogre2(X))),
+    asserta(quest(User,killogre2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 5 ->
     (Level >= 8 ->
-    asserta(quest(User,killwolf1(X))),
+    asserta(quest(User,killwolf1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 6 ->
     (Level >= 10 ->
-    asserta(quest(User,killwolf2(X))),
+    asserta(quest(User,killwolf2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 7 ->
     (Level >= 14 ->
-    asserta(quest(User,killgolem1(X))),
+    asserta(quest(User,killgolem1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 8 ->
     (Level >= 16 ->
-    asserta(quest(User,killgolem2(X))),
+    asserta(quest(User,killgolem2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 9 ->
     (Level >= 17 ->
-    asserta(quest(User,killviper1(X))),
+    asserta(quest(User,killviper1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 10 ->
     (Level >= 19 ->
-    asserta(quest(User,killviper2(X))),
+    asserta(quest(User,killviper2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 11 ->
     (Level >= 19 ->
-    asserta(quest(User,killsorrowling1(X))),
+    asserta(quest(User,killsorrowling1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 12 ->
     (Level >= 21 ->
-    asserta(quest(User,killsorrowling2(X))),
+    asserta(quest(User,killsorrowling2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 13 ->
     (Level >= 22 ->
-    asserta(quest(User,killaghanim1(X))),
+    asserta(quest(User,killaghanim1)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 14 ->
     (Level >= 25 ->
-    asserta(quest(User,killaghanim2(X))),
+    asserta(quest(User,killaghanim2)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!);
 
     Pickquest = 15 ->
     (Level >= 28 ->
-    asserta(quest(User,killaghanim3(X))),
+    asserta(quest(User,killaghanim3)),
     write('You\'ve chosen this quest!'),nl,!;
     write('You haven\'t met the required level!'),!)).
 
@@ -167,51 +167,51 @@ upgradingquest :-
     read_integer(Pickquest),nl,nl,
 
     (Pickquest = 1 ->
-    (asserta(quest(User,upgradewp1(X))),
+    (asserta(quest(User,upgradewp1)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 2 ->
-    (asserta(quest(User,upgradewp2(X))),
+    (asserta(quest(User,upgradewp2)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 3 ->
-    (asserta(quest(User,upgradewp3(X))),
+    (asserta(quest(User,upgradewp3)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 4 ->
-    (asserta(quest(User,upgradewp4(X))),
+    (asserta(quest(User,upgradewp4)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 5 ->
-    (asserta(quest(User,upgradear1(X))),
+    (asserta(quest(User,upgradear1)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 6 ->
-    (asserta(quest(User,upgradear2(X))),
+    (asserta(quest(User,upgradear2)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 7 ->
-    (asserta(quest(User,upgradear3(X))),
+    (asserta(quest(User,upgradear3)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 8 ->
-    (asserta(quest(User,upgradear4(X))),
+    (asserta(quest(User,upgradear4)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 9 ->
-    (asserta(quest(User,upgradeacc1(X))),
+    (asserta(quest(User,upgradeacc1)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 10 ->
-    (asserta(quest(User,upgradeacc2(X))),
+    (asserta(quest(User,upgradeacc2)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 11 ->
-    (asserta(quest(User,upgradeacc3(X))),
+    (asserta(quest(User,upgradeacc3)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 12 ->
-    (asserta(quest(User,upgradeacc4(X))),
+    (asserta(quest(User,upgradeacc4)),
     write('You\'ve chosen this quest!'),nl,!)).
 
 shoppingquest :-
@@ -228,19 +228,19 @@ shoppingquest :-
     read_integer(Pickquest),nl,nl,
 
     (Pickquest = 1 ->
-    (asserta(quest(User,buyhppotion(X))),
+    (asserta(quest(User,buyhppotion)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 2 ->
-    (asserta(quest(User,buybighppotion(X))),
+    (asserta(quest(User,buybighppotion)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 3 ->
-    (asserta(quest(User,buyattpotion(X))),
+    (asserta(quest(User,buyattpotion)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 4 ->
-    (asserta(quest(User,buydefpotion(X))),
+    (asserta(quest(User,buydefpotion)),
     write('You\'ve chosen this quest!'),nl,!)).
 
 gachaquest :-
@@ -260,270 +260,265 @@ gachaquest :-
     read_integer(Pickquest),nl,nl,
 
     (Pickquest = 1 ->
-    (asserta(quest(User,gacha5(X))),
+    (asserta(quest(User,gacha5)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 2 ->
-    (asserta(quest(User,gacha10(X))),
+    (asserta(quest(User,gacha10)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 3 ->
-    (asserta(quest(User,gacha15(X))),
+    (asserta(quest(User,gacha15)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 4 ->
-    (asserta(quest(User,gacha25(X))),
+    (asserta(quest(User,gacha25)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 5 ->
-    (asserta(quest(User,gacha50(X))),
+    (asserta(quest(User,gacha50)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 6 ->
-    (asserta(quest(User,gacha100(X))),
+    (asserta(quest(User,gacha100)),
     write('You\'ve chosen this quest!'),nl,!);
 
     Pickquest = 7 ->
-    (asserta(quest(User,gacha1000(X))),
+    (asserta(quest(User,gacha1000)),
     write('You\'ve chosen this quest!'),nl,!)).
+
+questdone(Quest) :-
+    rewardExp(Xp),
+    rewardGold(Gold),
+    retract(exp(X,Prev)),
+    retract(gold(X,Prev)),
+    New is Prev + Xp,
+    New is Prev + Gold,
+    asserta(exp(X,New)),
+    asserta(gold(X,New)).
 
 /* Quest Kill Monster */
 
-newbiemission1(X) :-
+newbiemission1 :-
     asserta(kill(goblin,15)),
-    asserta(level(X,1)),
-    asserta(rewardExp(X,100)),
-    asserta(rewardGold(X,100)).
+    asserta(rewardExp(100)),
+    asserta(rewardGold(100)).
 
-newbiemission2(X) :-
+newbiemission2 :-
     asserta(kill(slime,15)),
-    asserta(level(X,1)),
-    asserta(rewardExp(X,100)),
-    asserta(rewardGold(X,100)).
+    asserta(rewardExp(100)),
+    asserta(rewardGold(100)).
 
-killogre1(X) :-
+killogre1 :-
     asserta(kill(ogre,5)),
-    asserta(level(X,3)),
-    asserta(rewardExp(X,250)),
-    asserta(rewardGold(X,300)).
+    asserta(rewardExp(250)),
+    asserta(rewardGold(300)).
 
-killogre2(X) :-
+killogre2 :-
     asserta(kill(ogre,10)),
-    asserta(level(X,5)),
-    asserta(rewardExp(X,450)),
-    asserta(rewardGold(X,550)).
+    asserta(rewardExp(450)),
+    asserta(rewardGold(550)).
 
-killwolf1(X) :-
+killwolf1 :-
     asserta(kill(wolf,5)),
-    asserta(level(X,8)),
-    asserta(rewardExp(X,350)),
-    asserta(rewardGold(X,450)).
+    asserta(rewardExp(350)),
+    asserta(rewardGold(450)).
 
-killwolf2(X) :-
+killwolf2 :-
     asserta(kill(wolf,10)),
-    asserta(level(X,10)),
-    asserta(rewardExp(X,600)),
-    asserta(rewardGold(X,650)).
+    asserta(rewardExp(600)),
+    asserta(rewardGold(650)).
 
-killgolem1(X) :-
+killgolem1 :-
     asserta(kill(golem,5)),
-    asserta(level(X,14)),
-    asserta(rewardExp(X,600)),
-    asserta(rewardGold(X,600)).
+    asserta(rewardExp(600)),
+    asserta(rewardGold(600)).
 
-killgolem2(X) :-
+killgolem2 :-
     asserta(kill(golem,10)),
-    asserta(level(X,16)),
-    asserta(rewardExp(X,800)),
-    asserta(rewardGold(X,850)).
+    asserta(rewardExp(800)),
+    asserta(rewardGold(850)).
 
-killviper1(X) :-
+killviper1 :-
     asserta(kill(viper,5)),
-    asserta(level(X,17)),
-    asserta(rewardExp(X,720)),
-    asserta(rewardGold(X,800)).
+    asserta(rewardExp(720)),
+    asserta(rewardGold(800)).
 
-killviper2(X) :-
+killviper2 :-
     asserta(kill(viper,10)),
-    asserta(level(X,19)),
-    asserta(rewardExp(X,920)),
-    asserta(rewardGold(X,1000)).
+    asserta(rewardExp(920)),
+    asserta(rewardGold(1000)).
 
-killsorrowling1(X) :-
+killsorrowling1 :-
     asserta(kill(sorrowling,3)),
-    asserta(level(X,19)),
-    asserta(rewardExp(X,1060)),
-    asserta(rewardGold(X,1320)).
+    asserta(rewardExp(1060)),
+    asserta(rewardGold(1320)).
 
-killsorrowling2(X) :-
+killsorrowling2 :-
     asserta(kill(sorrowling,5)),
-    asserta(level(X,21)),
-    asserta(rewardExp(X,1640)),
-    asserta(rewardGold(X,1880)).
+    asserta(rewardExp(1640)),
+    asserta(rewardGold(1880)).
 
-killaghanim1(X) :-
+killaghanim1 :-
     asserta(kill(aghanim,1)),
-    asserta(level(X,22)),    
-    asserta(rewardExp(X,1800)),
-    asserta(rewardGold(X,2400)).
+    asserta(rewardExp(1800)),
+    asserta(rewardGold(2400)).
 
-killaghanim2(X) :-
+killaghanim2 :-
     asserta(kill(aghanim,3)),
-    asserta(level(X,25)),
-    asserta(rewardExp(X,2500)),
-    asserta(rewardGold(X,3000)).
+    asserta(rewardExp(2500)),
+    asserta(rewardGold(3000)).
 
-killaghanim3(X) :-
+killaghanim3 :-
     asserta(kill(aghanim,5)),
-    asserta(level(X,28)),
-    asserta(rewardExp(X,4000)),
-    asserta(rewardGold(X,5900)).
+    asserta(rewardExp(4000)),
+    asserta(rewardGold(5900)).
 
 /* Quest Upgrade Item */
 
-upgradewp1(Item) :-
+upgradewp1 :-
     class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
     Level == wood ->
-    (asserta(rewardExp(X,250))),
-    (asserta(rewardGold(X,250))).
+    (asserta(rewardExp(250))),
+    (asserta(rewardGold(250))).
 
-upgradewp2(Item) :-
+upgradewp2 :-
     class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
     Level == iron ->
-    (asserta(rewardExp(X,500))),
-    (asserta(rewardGold(X,500))).
+    (asserta(rewardExp(500))),
+    (asserta(rewardGold(500))).
 
-upgradewp3(Item) :-
+upgradewp3 :-
     class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
     Level == diamond ->
-    (asserta(rewardExp(X,1000))),
-    (asserta(rewardGold(X,1000))).
+    (asserta(rewardExp(1000))),
+    (asserta(rewardGold(1000))).
 
-upgradewp4(Item) :-
+upgradewp4 :-
     class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
     Level == legendary ->
-    (asserta(rewardExp(X,1650))),
-    (asserta(rewardGold(X,1650))).
+    (asserta(rewardExp(1650))),
+    (asserta(rewardGold(1650))).
 
-upgradear1(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradear1 :-
+    class(User,Class), armorcheck(Item,Class), itemlevel(Item,Level),
     Level == wood ->
-    (asserta(rewardExp(X,250))),
-    (asserta(rewardGold(X,250))).
+    (asserta(rewardExp(250))),
+    (asserta(rewardGold(250))).
 
-upgradear2(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradear2 :-
+    class(User,Class), armorcheck(Item,Class), itemlevel(Item,Level),
     Level == iron ->
-    (asserta(rewardExp(X,500))),
-    (asserta(rewardGold(X,500))).
+    (asserta(rewardExp(500))),
+    (asserta(rewardGold(500))).
 
-upgradear3(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradear3 :-
+    class(User,Class), armorcheck(Item,Class), itemlevel(Item,Level),
     Level == diamond ->
-    (asserta(rewardExp(X,1000))),
-    (asserta(rewardGold(X,1000))).
+    (asserta(rewardExp(1000))),
+    (asserta(rewardGold(1000))).
 
-upgradear4(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradear4 :-
+    class(User,Class), armorcheck(Item,Class), itemlevel(Item,Level),
     Level == legendary ->
-    (asserta(rewardExp(X,1650))),
-    (asserta(rewardGold(X,1650))).
+    (asserta(rewardExp(1650))),
+    (asserta(rewardGold(1650))).
 
-upgradeacc1(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradeacc1 :-
+    class(User,Class), accessorycheck(Item,Class), itemlevel(Item,Level),
     Level == wood ->
-    (asserta(rewardExp(X,250))),
-    (asserta(rewardGold(X,250))).
+    (asserta(rewardExp(250))),
+    (asserta(rewardGold(250))).
 
-upgradeacc2(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradeacc2 :-
+    class(User,Class), accessorycheck(Item,Class), itemlevel(Item,Level),
     Level == iron ->
-    (asserta(rewardExp(X,500))),
-    (asserta(rewardGold(X,500))).
+    (asserta(rewardExp(500))),
+    (asserta(rewardGold(500))).
 
-upgradeacc3(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradeacc3 :-
+    class(User,Class), accessorycheck(Item,Class), itemlevel(Item,Level),
     Level == diamond ->
-    (asserta(rewardExp(X,1000))),
-    (asserta(rewardGold(X,1000))).
+    (asserta(rewardExp(1000))),
+    (asserta(rewardGold(1000))).
 
-upgradeacc4(Item) :-
-    class(User,Class), weaponcheck(Item,Class), itemlevel(Item,Level),
+upgradeacc4 :-
+    class(User,Class), accessorycheck(Item,Class), itemlevel(Item,Level),
     Level == legendary ->
-    (asserta(rewardExp(X,1650))),
-    (asserta(rewardGold(X,1650))).
+    (asserta(rewardExp(1650))),
+    (asserta(rewardGold(1650))).
 
 /* Quest Shop */
 
-buyhppotion(Item) :-
-    asserta(buy(Item,20)),
-    asserta(rewardExp(X,150)),
-    asserta(rewardGold(X,250)).
+buyhppotion :-
+    asserta(buy(health_potion,20)),
+    asserta(rewardExp(150)),
+    asserta(rewardGold(250)).
 
-buybighppotion(Item) :-
-    asserta(buy(Item,10)),
-    asserta(rewardExp(X,150)),
-    asserta(rewardGold(X,250)).
+buybighppotion :-
+    asserta(buy(big_health_potion,10)),
+    asserta(rewardExp(150)),
+    asserta(rewardGold(250)).
 
-buyattpotion(Item) :-
-    asserta(buy(Item,15)),
-    asserta(rewardExp(X,300)),
-    asserta(rewardGold(X,300)).
+buyattpotion :-
+    asserta(buy(attack_potion,15)),
+    asserta(rewardExp(300)),
+    asserta(rewardGold(300)).
 
-buydefpotion(Item) :-
-    asserta(buy(Item,15)),
-    asserta(rewardExp(X,300)),
-    asserta(rewardGold(X,300)).
+buydefpotion :-
+    asserta(buy(defense_potion,15)),
+    asserta(rewardExp(300)),
+    asserta(rewardGold(300)).
 
 /* Quest Gacha */
 
-gacha5(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,500)),
-    asserta(rewardGold(X,500)),
+gacha5 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(500)),
+    asserta(rewardGold(500)),
     keepitem(health_potion,10),
     keepitem(big_health_potion,5),
     keepitem(attack_potion,5),
     keepitem(defense_potion,5).
 
-gacha10(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,500)),
-    asserta(rewardGold(X,500)),
+gacha10 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(500)),
+    asserta(rewardGold(500)),
     randomwood.
 
-gacha15(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,600)),
-    asserta(rewardGold(X,600)),
+gacha15 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(600)),
+    asserta(rewardGold(600)),
     randomiron.
 
-gacha25(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,800)),
-    asserta(rewardGold(X,800)),
+gacha25 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(800)),
+    asserta(rewardGold(800)),
     randomdiamond.
 
-gacha50(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,1200)),
-    asserta(rewardGold(X,1200)),
+gacha50 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(1200)),
+    asserta(rewardGold(1200)),
     randomlegendary.
 
-gacha100(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,2500)),
-    asserta(rewardGold(X,2500)),
+gacha100 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(2500)),
+    asserta(rewardGold(2500)),
     randomwood,
     randomiron,
     randomdiamond,
     randomlegendary,
     write('You are insane!').
 
-gacha1000(X) :-
-    asserta(gacha(X,10)),
-    asserta(rewardExp(X,10000)),
-    asserta(rewardGold(X,10000)),
+gacha1000 :-
+    asserta(gacha(10)),
+    asserta(rewardExp(10000)),
+    asserta(rewardGold(10000)),
     randomlegendary,
     randomlegendary,
     randomlegendary,
